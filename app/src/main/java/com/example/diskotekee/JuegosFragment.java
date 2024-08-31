687f0c22b52ce3c6654b04382925f51fcf479e9e
SrWalls17 <xbenjaprds17@gmail.com>
2024-08-30 22:31:21 -0400

    Arreglo parte visual
    Se implemento mejora visual de la app.

--- app/src/main/java/com/example/diskotekee/JuegosFragment.java
+++ app/src/main/java/com/example/diskotekee/JuegosFragment.java
@@ -1,34 +1,46 @@
 package com.example.diskotekee;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
 public class JuegosFragment extends Fragment {
+
     public JuegosFragment() {
         // Required empty public constructor
     }
-    public static JuegosFragment newInstance(String param1, String param2) {
-        JuegosFragment fragment = new JuegosFragment();
-        Bundle args = new Bundle();
-        return fragment;
-    }
 
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_juegos, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Find the ImageView by their IDs
+        View imageViewRuleta = view.findViewById(R.id.imageView);
+        View imageViewVerdadReto = view.findViewById(R.id.imageView2);
+
+        // Set onClick listeners
+        imageViewRuleta.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), Ruleta.class);
+            startActivity(intent);
+        });
+
+        imageViewVerdadReto.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), VerdadoReto.class);
+            startActivity(intent);
+        });
     }
 }
---
+++ app/src/main/java/com/example/diskotekee/Ruleta.java
@@ -0,0 +1,14 @@
+package com.example.diskotekee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ruleta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ruleta);
+    }
+}
