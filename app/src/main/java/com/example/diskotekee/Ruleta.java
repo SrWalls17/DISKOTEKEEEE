853ee0520dca38535a30a3e2ed0e1f2781d72bd3
SrWalls17 <xbenjaprds17@gmail.com>
2024-09-02 21:14:35 -0400

    Desarrollo de la Ruleta
    Desarrollo de la funcionalidad del mini juego de la ruleta, incluyendo animación de giro e interacción con el usuario

--- app/src/main/java/com/example/diskotekee/Ruleta.java
+++ app/src/main/java/com/example/diskotekee/Ruleta.java
@@ -3,12 +3,84 @@ package com.example.diskotekee;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class Ruleta extends AppCompatActivity {
+
+    private static final String [] sectors = {"1","2","3","4","5","6","7","8","9","10","11","12",};
+    private static final int [] sectorDegrees= new int [sectors.length];
+    private int degree = 0;
+    private boolean isSpinning = false;
+    private static final Random random = new Random();
+
+    private ImageView wheel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ruleta);
+
+        final ImageView spinBtn = findViewById(R.id.spinbtn);
+        wheel = findViewById(R.id.wheel);
+
+        getDegreeForSectors();
+
+        spinBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isSpinning){
+                    spin();
+                    isSpinning=true;
+
+                }
+            }
+        });
+    }
+
+    private void spin(){
+        degree = random.nextInt(sectors.length-1);
+
+        RotateAnimation rotateAnimation = new RotateAnimation(0,(360 * sectors.length)+ sectorDegrees[degree],
+                RotateAnimation.RELATIVE_TO_SELF,0.5f, RotateAnimation.RELATIVE_TO_SELF, 0.5f);
+
+        rotateAnimation.setDuration(3600);
+        rotateAnimation.setFillAfter(true);
+        rotateAnimation.setInterpolator(new DecelerateInterpolator());
+        rotateAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Toast.makeText(Ruleta.this, "Tienes "+sectors[sectors.length-(degree+1)]+" puntos", Toast.LENGTH_SHORT).show();
+                isSpinning=false;
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        wheel.startAnimation(rotateAnimation);
+
+    }
+
+    private void getDegreeForSectors(){
+        int sectorDegree = 360/ sectors.length;
+        for(int i =0; i < sectors.length;i++){
+
+            sectorDegrees[i]=(i+1)* sectorDegree;
+        }
     }
 }
